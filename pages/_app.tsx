f41c91e7111a279c46eda4eb4ef6f3673b27e5e8
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-03-16 18:07:06 +0600

    add: Setup base app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
+
+import theme from "$theme/index";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </RecoilRoot>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
