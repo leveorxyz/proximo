c29c0c29f657c0936af3886b5321b903c4100c4a
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-03-16 18:04:10 +0600

    add: Create theme file

---
+++ theme/index.ts
@@ -0,0 +1,12 @@
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react"
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+export default extendTheme({
+  config,
+  fonts: {},
+  colors: {},
+})
